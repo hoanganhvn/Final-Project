3b7f915be2601d399a9dff2dcdd68f0e1044b3a3
{Hoàng <{nguyenhoanganhvn90@gmail.com}>
2022-01-28 05:37:05 +0700

    update mobile

--- finalproject.js
+++ finalproject.js
@@ -83,3 +83,11 @@ submit.addEventListener("click", function () {
         warning2.style.display = "block";
     }
 })
+
+// Mini menu
+let bars = document.getElementById("bars");
+
+bars.addEventListener("click",function(){
+    let navlist = document.querySelector(".navlist");
+    navlist.classList.toggle("active");
+});
