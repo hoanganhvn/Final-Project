01b3766d85b184e17cbd71ed188e36571e0db309
Phong Luong <luonghongphong6703@gmail.com>
2022-02-14 01:17:44 +0700

    Add Countdown Timer

--- LandingPage.script.js
+++ LandingPage.script.js
@@ -147,38 +147,45 @@ li30.addEventListener("click", function () {
 })
 
 
-
-
-
-
-
-
-
-
-
-
-let future = new Date('February 28,2022 00:00:00').getTime();
-setInterval(function () {
-    let present = new Date().getTime();
-    let delta = future - present;
-    let days = Math.floor(delta / (1000 * 60 * 60 * 24));
-    let hours = Math.floor(delta / (1000 * 60 * 60));
-    let minutes = Math.floor(delta / (1000 * 60));
-    let seconds = Math.floor(delta / 1000);
-
-    hours %= 24;
-    minutes %= 60;
-    seconds %= 60;
-
-    document.getElementsByClassName("days").innerText = days;
-    document.getElementsByClassName("hours").innerText = hours;
-    document.getElementsByClassName("minutes").innerText = minutes;
-    document.getElementsByClassName("seconds").innerText = seconds;
-
-    console.log(present);
-    console.log(future);
-    console.log(seconds);
-    console.log(delta);
-    console.log(hours);
-    console.log(minutes);
-}, 1000)
+function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const daysSpan = clock.querySelector('.days');
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+  
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+  
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  const deadline = new Date('February 28, 2022 00:00:00');
+  initializeClock('clockdiv', deadline);
